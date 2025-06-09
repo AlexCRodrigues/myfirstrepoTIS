54352b3f9eb5cad8a563f874e96f91a2f26e1ae4
AlexCRodrigues <alex.c.rodrigues207@gmail.com>
2025-06-09 20:33:46 +0000

    Add files via upload
    new "website"

---
+++ coiso.js
@@ -0,0 +1,16 @@
+function mostrarMensagem() {
+    const msg = document.getElementById("troll-msg");
+    msg.innerText = "mano achavas msm que isto iria funcionar?";
+    msg.style.display = "block";
+  }
+  function mostrarMensagem2() {
+    const msg = document.getElementById("troll-msg");
+    msg.innerText = "Bro this pc can't even run this game at like 1fps and you wanna see gameplay? the hell is wrong with you?";
+    msg.style.display = "block";
+  }
+  
+  function mostrarMensagem3() {
+    const msg = document.getElementById("troll-msg");
+    msg.innerText = "This benchmark was powered by dreams and desperation. The OS barely boots.";
+    msg.style.display = "block";
+  }
