1b99d575d9e3744b8b79e12bea84761ece4799a4
AlexCRodrigues <alex.c.rodrigues207@gmail.com>
2025-06-10 15:19:22 +0000

    Add files via upload

---
+++ Projects/coiso.js
@@ -0,0 +1,16 @@
+function mostrarMensagem() {
+    const msg = document.getElementById("troll-msg");
+    msg.innerText = "Did you really think this would work?";
+    msg.style.display = "block";
+  }
+  function mostrarMensagem2() {
+    const msg = document.getElementById("troll-msg");
+    msg.innerText = "Bro this pc can't even run this game at like 1fps and you wanna see gameplay? the hell is wrong with you?";
+    msg.style.display = "block";
+  }
+  
+  function mostrarMensagem3() {
+    const msg = document.getElementById("troll-msg");
+    msg.innerText = "This benchmark was powered by dreams and desperation. The OS barely boots.";
+    msg.style.display = "block";
+  }
---
+++ Projects/ps6.js
@@ -0,0 +1,6 @@
+function mostrarMeme(event) {
+     event.preventDefault();
+    const msg = document.getElementById("troll-msg");
+    msg.innerText = "Bro can't afford to pay the bills but thinks he can pay 100K on this stupid ass console";
+    msg.style.display = "block";
+  }
